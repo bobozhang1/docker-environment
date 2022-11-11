a6fc9fb53cde941a97a25e86e096777e64e2765a
boyd <zsir936484@163.com>
2022-11-11 15:34:53 +0800

    home

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,33 @@
-import { defineUserConfig } from 'vuepress'
+import {defaultTheme, defineUserConfig} from 'vuepress'
 
 export default defineUserConfig({
     lang: 'zh-CN',
-    title: 'boyd',
+    title: '狂奔的蜗牛',
     description: 'boyd  的文档',
-    base: 'docker-environment/'
+    base: 'docker-environment/',
+    head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
+    theme: defaultTheme({
+        logo: '/images/logo.png',
+        navbar: [
+            // NavbarItem
+            {
+                text: '开发规范',
+                link: '/specification/'
+            },
+            // NavbarGroup
+            {
+                text: '数据库',
+                children: [{
+                    text: 'MYSQL',
+                    link: '/database/mysql.md',
+                    activeMatch: '/',
+                }, {
+                    text: 'REDIS',
+                    link: '/database/redis.md',
+                    activeMatch: '/',
+                }],
+            },
+        ],
+    }),
+
 })
